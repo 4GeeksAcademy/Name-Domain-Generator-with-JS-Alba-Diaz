674091a7827425b19b2a6e3eeba2b4088409dbd5
Alba Diaz <alba.dilop@gmail.com>
2025-03-14 22:50:12 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our']; //arr1 --> i
+  let adj = ['great', 'big', 'fantastic']; //arr2 --> j
+  let noun = ['jogger', 'racoon', 'bayonet', 'aches']; //arr3 --> k
+  let domain = ['.com', '.es', '.net'] //arr4 ---> l
+
+//function listOfEMails() { esto es el extra 
+  for (let i in pronoun){
+    for (let j in adj){  
+      for (let k in noun) {
+        for (let l in domain) {
+           pronoun[i] + adj[j] + noun[k] + domain[l];
+           // si el final del los elementos del array [noun] es igual al final de los elementos del array [domain] 
+           // entonces genera un string en el que las dos ultimas letras de los elementos del array [noun] desaparezcan 
+          // if 
+          
+        console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);        
+          
+        }
+      }
+    }
+  }
+ // return (extra)
+// }
+
+
+//document.getElementById("email").innerHTML = listOfEMails(); (extra)
+
+
+
 };
+
